be2456faf409f66a718d91be090a41be7a537ca9
Joost <github@joostnieuweboer.nl>
2025-05-05 14:36:09 +0000

    Added better printing of weaknesses, added abilities to weaknesses

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
+#usage
 #python3 main.py
 
 #imports
 import sys
-from pokemon_functions import describe_pokemon, get_families
+#import own functions
+from pokemon_functions import describe_pokemon, describe_pokemon_all, get_families
+
+
 
 #main function
 def main():
     #if not correct arguments
     if len(sys.argv) < 2:
+        #print usage
         print_usage()
 
     #get information
     command = sys.argv[1].lower()
     #check what to do
     if command == "pokemon":
+        #arg 3 = specific pokemon
         if len(sys.argv) < 3:
             print_usage()
         else:
             pokemon = sys.argv[2].lower()
-            describe_pokemon(pokemon)
-
+            #if they want to have all pokemon
+            if pokemon == "all":
+                describe_pokemon_all()
+            #searching for a specific pokemon
+            else:
+                describe_pokemon(pokemon)
+    #get the pokemon families
     elif command == "families":
         get_families()
-
+    #not a valid command
     else:
+        #print usage
         print_usage()
+    #stop function
     return
 
 
@@ -33,12 +46,14 @@ def main():
 def print_usage():
     #print help message
     print("Usage: python3 main.py <command>")
-    print("pokemon <name or number> -> gets information of the pokemon")
+    print("pokemon <name/number> -> gets information of the pokemon, inputting 'all' gets information of all pokemon in the pokedex !!!")
     print("families -> gets information of the pokemon families")
     #stop
     sys.exit(1)
     #needed?
     return
+
+
 
 #exectue main function
 main()
--- pokemon_functions.py
+++ pokemon_functions.py
@@ -1,12 +1,16 @@
 #imports
 import csv, sys
+from termcolor import colored, cprint
+from type_fuctions import get_weakness
 
 
+#https://pypi.org/project/termcolor/
 
 #constants
 ___NUMBER_OF_POKEMON___ = 151 #1010
 ___CSV_POKEMON____ = "pokemon.csv"
-___CSV_TYPES___ = "types.csv"
+
+
 
 #Return data, according to input
 #either number or name
@@ -18,6 +22,8 @@ def get_pokemon_data(input_raw):
     dict_keys = "name"
     #check if name input
     if input_sanatized.isdigit():
+        #remove the first '0' by converting to int (and then back to str)
+        input_sanatized = str(int(input_sanatized))
         dict_keys = "number"
     #debug
     #print(f"get_pokemon_data '{dict_keys}' '{input_sanatized}'")
@@ -33,6 +39,8 @@ def get_pokemon_data(input_raw):
                 if row[dict_keys].lower() == input_sanatized:
                     #convert the data to an object
                     pokemon_object = pokemon(row)
+                    #debug
+                    #print(f"Found pokemon for '{input_sanatized}': {pokemon_object.name}")
                     #return the object
                     return pokemon_object, None 
         #if exception
@@ -50,17 +58,26 @@ def get_families():
     families = []
     print("Pokemon families:")
     for index in range(1, ___NUMBER_OF_POKEMON___):
+        #convert to string for lookup
         index = str(index)
-        #print(f"index not in numbers_processed: {index} = {index not in numbers_processed}")
+        #if not already processed
         if index not in numbers_processed:
+            #get the evolution line
             evolution_tree, evolution_numbers = get_evolution_line(index)
+            #print the evolution line
             print(f"{", ".join(evolution_tree)}")
             #families.append(evolution_tree)
             numbers_processed = numbers_processed + evolution_numbers
-            #print(numbers_processed)
-        #print(f"{'\r\n'.join(families)}")
+
     
 
+#describe all pokemon
+def describe_pokemon_all():
+    #for every pokemon
+    for index in range(1, ___NUMBER_OF_POKEMON___):
+        pokemon = str(index)
+        #describe the pokemon
+        describe_pokemon(pokemon)
 
 
 
@@ -76,6 +93,7 @@ def get_evolution_line(input):
     if error is not None:
         print(f"get_evolution_line: {error}")
     else:
+        #print(f"Start get_evolutions: get_base_pokemon('{input}')")
         #get the evolution tree
         get_evolutions(base_pokemon, evolution_tree, evolution_numbers)
     #return data
@@ -85,14 +103,29 @@ def get_evolution_line(input):
 
 #[names, numbers], error
 def get_evolutions(pokemon_object, evolution_tree, evolution_numbers):
-    evolution_tree.append(pokemon_object.name)
+    #guard clause, check object
+    if not hasattr(pokemon_object, 'name'):
+        print(f"Pokemon object")
+
+    #determine the name of the pokemon
+    name = pokemon_object.name
+    #if it is a specific form:
+    if pokemon_object.form != "":
+        name += " " + pokemon_object.form
+    #add information of the current pokemon
+    evolution_tree.append(name)
     evolution_numbers.append(pokemon_object.number)
     #for each successor
     for evolution in pokemon_object.successors:
         #get object
         successor, error = get_pokemon_data(evolution)
-        #go deeper
-        get_evolutions(successor, evolution_tree, evolution_numbers)
+        #if error occurred
+        if error is not None:
+            #print error
+            print(f"get_evolutions: {error}")
+        else:
+            #go deeper
+            get_evolutions(successor, evolution_tree, evolution_numbers)
     return
 
 
@@ -114,52 +147,6 @@ def get_base_pokemon(input):
     return None, "get_base_pokemon: Pokemon not found!"
 
 
-#assuming object input
-def get_weakness(input):
-    #keep track of scores
-    score = {
-        4: [],
-        2: [],
-        1: [],
-        0.5: [],
-        0.25: [],
-        0: [],
-    }
- 
-    #used multiple times
-    filename = ___CSV_TYPES___
-    #use the csv as data source
-    with open(filename) as csvfile: 
-        #use a dict
-        reader = csv.DictReader(csvfile)
-        try:
-            for row in reader:
-                attack_type = row["Type"] 
-                
-                multiplier_1 = row[input.type[0]]
-                if multiplier_1 == "":
-                    multiplier_1 = float(1)
-                else:
-                    multiplier_1 = float(multiplier_1)
-                damage_multiplier = multiplier_1
-                
-                if len(input.type) > 1 and input.type[1] != "":
-                    multiplier_2 = row[input.type[1]]
-                    if multiplier_2 == "":
-                        multiplier_2 = float(1)
-                    else:
-                        multiplier_2 = float(multiplier_2)
-                    damage_multiplier = damage_multiplier * multiplier_2
-                
-                #add to score list
-                score[damage_multiplier].append(attack_type)
-        
-        #if exception
-        except csv.Error as e:
-            #print and exit
-            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
-    #default return value
-    return score          
 
 
 
@@ -174,28 +161,66 @@ def describe_pokemon(input):
         print(error)
         #stop
         return
-    
     #print report
-    print(f"Pokemon #{pokemon.number}: {pokemon.name}")
-    
-    
+    #describe the name
+    name = pokemon.name
+    #if it is a specific form
+    if pokemon.form != "":
+        #add to name
+        name += f" ({pokemon.form})"
+    #print the number and name
+    cprint(f"Pokemon #{pokemon.number}: {pokemon.name}", None, attrs=["bold", "underline"])
+    #print types
+    type_string = "Type: "
+     #if there is only 1 ability
+    if pokemon.type[1] == "":
+        #only print the first one
+        type_string += f"{"".join(pokemon.type)}"
+    else: 
+        #print both abilities
+        type_string += f"{", ".join(pokemon.type)}"
 
-    print(f"Type: {", ".join(pokemon.type)}")
+    print(f"{type_string}")
+    #get the abilities
     ability_string = "Abilities: "
+    #if there is only 1 ability
     if pokemon.abilities[1] == "":
+        #only print the first one
         ability_string += f"{"".join(pokemon.abilities)}"
     else: 
+        #print both abilities
         ability_string += f"{", ".join(pokemon.abilities)}"
-    
+    #if there is a hidden abilty
     if pokemon.hidden_ability != "": 
+        #add the hidden ability
         ability_string += f" ({pokemon.hidden_ability})"
+    
+    #print abilities
     print(ability_string)
 
-    weakness = get_weakness(pokemon)
-    print(f"Weakness: {weakness}")
-
+    #print evolution tree
     evolution_tree, _ = get_evolution_line(pokemon.name)
-    print(f"Evolution tree: {", ".join(evolution_tree)}")
+    print(f"Evolutions: {", ".join(evolution_tree)}")
+
+    #get the type match-up
+    weaknesses = get_weakness(pokemon)
+    
+    #for each
+    for weakness in weaknesses:
+        modifier = weakness[0]
+        types = ", ".join(weakness[1])
+       
+        #print(f"weakness key: {key} = {0 < value < 1}")
+        if modifier > float(1): 
+            cprint(f"Super Effective ({modifier}): {types}", "green")
+        elif modifier <= float(0):
+            cprint(f"Immune ({modifier}): {types}", "red", attrs=["bold","underline"])
+        elif modifier != float(1):
+            cprint(f"Not very effective ({modifier}): {types}", "red")   
+
+    #empty line
+    print("")
+
 
 
 #pokemon object, containing information of the pokemon
